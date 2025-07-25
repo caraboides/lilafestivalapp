ffd7af4fe6dd29704d642205b0ae6e979bde7d54
Daniel Scheibe <daniel.scheibe@hagebau.com>
2025-07-25 13:04:37 +0200

    logo and splash update

--- lib/main_spirit.dart
+++ lib/main_spirit.dart
@@ -1,9 +1,7 @@
+import 'package:intl/intl.dart';
 import 'flavor_spirit/module.dart';
-import 'package:flutter_localizations/flutter_localizations.dart'; // falls nötig
-import 'package:intl/intl.dart'; // nur falls du intl nutzt
-import 'package:i18n_extension/i18n_extension.dart';  // für I18n.locale
 import 'main.dart';
-import 'main.dart';
+
 
 void main() {
 
